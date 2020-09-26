55ced444314162004ff7db94863f7d3e0892d1ba
Saravanan raghul <saravananraghul1820@gmail.com>
2020-09-26 22:25:49 +0530

    release 1.0.0

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const passport = require("passport");
+
+const package = require("../package.json");
+const auth = require("./auth");
+
+module.exports = (app) => {
+    app.get("/", passport.authenticate("jwt", {
+        session: false
+    }), (req, res) => {
+        res.send({
+            version: package.version
+        });
+    });
+
+    auth(app);
+};
